4e6607777e6ff02743fc7153009ef8bcdb52ab61
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-10-18 21:43:09 +0300

    adding the color list of the cars

---
+++ car_color.py
@@ -0,0 +1,38 @@
+car_colors = [
+    # Basic colors
+    "black", "white", "red", "green", "blue", "yellow", "cyan", "magenta",
+
+    # Gray tones
+    "gray", "lightgray", "darkgray", "dimgray", "slategray", "gainsboro",
+
+    # Browns and earth tones
+    "brown", "chocolate", "sienna", "saddlebrown", "tan", "wheat", "beige", "burlywood",
+
+    # Oranges and warm tones
+    "orange", "darkorange", "coral", "tomato", "salmon", "lightsalmon", "peachpuff",
+
+    # Pinks and purples
+    "pink", "lightpink", "deeppink", "hotpink", "violet", "plum",
+    "orchid", "lavender", "thistle", "magenta", "purple", "indigo",
+
+    # Blues
+    "blue", "deepskyblue", "skyblue", "lightblue", "dodgerblue",
+    "steelblue", "royalblue", "navy", "slateblue", "mediumblue", "darkblue",
+
+    # Greens
+    "green", "lime", "forestgreen", "darkgreen", "seagreen",
+    "lightgreen", "mediumseagreen", "springgreen", "lawngreen", "palegreen",
+
+    # Yellows
+    "yellow", "lightyellow", "lemonchiffon", "khaki", "gold", "goldenrod", "darkkhaki",
+
+    # Aquas / turquoises
+    "aqua", "turquoise", "darkturquoise", "mediumturquoise",
+    "paleturquoise", "cadetblue", "teal", "aquamarine",
+
+    # Reds
+    "firebrick", "crimson", "darkred", "indianred",
+
+    # Extras
+    "azure", "mintcream", "honeydew", "snow", "ivory", "linen", "mistyrose"
+]
