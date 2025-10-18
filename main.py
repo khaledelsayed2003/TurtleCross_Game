9456e29cc9acf2817cdc61a4205bbe8b152daae0
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-10-18 17:20:52 +0300

    refactor(player): move Player logic to OOP-based class using inheritance

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from turtle import Turtle, Screen
+from player import Player
+import time
 
 screen = Screen()
 screen.setup(width= 600, height= 600)
 screen.title("TurtleCross — A Python Turtle Game")
+screen.tracer(0)
 
 
-
-turtle = Turtle()
-turtle.shape("turtle")
-turtle.penup()
-turtle.setheading(90)
-turtle.goto(0, -280)
-
-
-def move_forward():
-    turtle.forward(10)
-
+player = Player()
 
 screen.listen()
-screen.onkey(fun= move_forward, key="Up")
+screen.onkey(fun= player.move_forward, key="Up")
 
 
 
 
+
+game_over = False
+while not game_over:
+    time.sleep(0.07)
+    screen.update()
+    
+    
+    
 
 
 
---
+++ player.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(0, -280)
+    
+    def move_forward(self):
+        self.forward(20)
