3f8308ec7f85d193c34858bde8f31d0ff6369c4d
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-10-18 21:41:15 +0300

    Updating player class

--- player.py
+++ player.py
@@ -1,12 +1,19 @@
 from turtle import Turtle
 
+STARTING_POSITION =(0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+
 class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("turtle")
         self.penup()
         self.setheading(90)
-        self.goto(0, -280)
+        self.goto(STARTING_POSITION)
     
-    def move_forward(self):
-        self.forward(20)
+    
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
